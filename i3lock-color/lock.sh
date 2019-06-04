f1d6e486bb7cf9d6511c9627e9bc74692983991c
biancamocanu <bianca.mocanu@uconn.edu>
2019-06-04 15:55:54 +0200

    files as of June 2019

---
+++ i3lock-color/lock.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+
+/home/bianca/i3lock-color/x86_64-pc-linux-gnu/i3lock \
+--blur=10 \
+--clock \
+--indicator \
+--insidecolor=232C31FF \
+--ringcolor=9EA7A6FF \
+--line-uses-inside \
+--keyhlcolor=2A5491FF \
+--bshlcolor=A03B1EFF \
+--insidevercolor=232C31FF \
+--insidewrongcolor=A03B1Eff \
+--ringvercolor=9EA7A6FF \
+--ringwrongcolor=3F4944FF \
+--separatorcolor=2A5491FF \
+--verifcolor=FFFFFFFF \
+--wrongcolor=232C31FF \
+--timecolor=9EA7A6FF \
+--datecolor=9EA7A6FF \
+
+
+# B='#00000000'  # blank
+# C='#ffffff22'  # clear ish
+# D ='#ff00ffcc'  # default
+# T='#ee00eeee'  # text
+# W='#880000bb'  # wrong
+# V='#bb00bbbb'  # verifying
+
+#--insidevercolor=$C   \
+#--ringvercolor=$V     \
+#\
+#--insidewrongcolor=$C \
+#--ringwrongcolor=$W   \
+#\
+#--insidecolor=$B      \
+#--ringcolor=$D        \
+#--linecolor=$B        \
+#--separatorcolor=$D   \
+#\
+#--verifcolor=$T        \
+#--wrongcolor=$T        \
+#--timecolor=$T        \
+#--datecolor=$T        \
+#--layoutcolor=$T      \
+#--keyhlcolor=$W       \
+#--bshlcolor=$W        \
+#\
+#--screen 1            \
+#--blur 5              \
+#--clock               \
+#--indicator           \
+#--timestr="%H:%M:%S"  \
+#--datestr="%A, %m %Y" \
+#--keylayout 2         \
+
+ --veriftext="Verifying..."
+ --wrongtext="Nope!"
+# --textsize=20
+# --modsize=10
+# --timefont=comic-sans
+# --datefont=monofur
+# etc
